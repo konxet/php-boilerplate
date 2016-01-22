bcc0f84b2a7969d3b1a0015c176fca5305986fb8
Zach Baldwin <Zach Baldwin>
2016-01-21 18:35:21 -0600

    Initial fixing.

---
+++ require/require.php
@@ -0,0 +1,24 @@
+<!-- JQUERY -->
+<script src="../js/jquery-2.2.0.min.js" type="text/javascript"></script>
+
+<!-- NORMALIZE -->
+<link href="../css/normalize.min.css" rel="stylesheet">
+
+<!-- MATERIALIZE -->
+<link href="../css/materialize.min.css" rel="stylesheet">
+<script src="../js/materialize.min.js" rel="stylesheet"></script>
+
+<!-- SIDR -->
+<link href="../css/jquery.sidr.dark.min.css" rel="stylesheet">
+<script src="../js/jquery.sidr.min.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+    $(document).ready(function()
+    {
+        $("#toggle-menu").sidr();
+    });
+</script>
+
+<!-- CUSTOM -->
+<link href="../css/main.css" rel="stylesheet">
+<script src="../js/main.js"></script>
